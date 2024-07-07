e0b9248aff31c94a68b95ddb39b24236b95b814d
tedthetrumpet <tedthetrumpet@gmail.com>
2024-07-07 23:33:53 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,24 @@ export default function Hello() {
     <Layout title="J Simon van der Walt" description="Composer, Performer, Teacher">
       <div
         style={{
+          position: 'relative',
+          margin: 'auto',
           display: 'flex', /* probably redundant */
           justifyContent: 'center',
           alignItems: 'center',
           /* height: '60vh', */
           /* fontSize: '20px', */
+          width: '90%'
         }}>
         <img src="/img/jsimonvanderwalt.png" alt="Simon"></img>
         <div
           style={{
+/*             margin: 'auto',
+            width: '90%', */
             color: 'white',
             position: 'absolute',
-            left: 25,
-            top: 50,
+            left: 40,
+            top: 0,
           }}>
           <p style={{ fontSize: '5.1vw'}}>J Simon van der Walt</p>
           <p style={{ fontSize: '3.4vw', lineHeight: 0.05 }}>composer | performer | teacher</p>
