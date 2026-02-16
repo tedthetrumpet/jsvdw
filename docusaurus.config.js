8ba8bc3347b6aa38c3ca1e4d580a7eee48ef91ef
tedthetrumpet <tedthetrumpet@gmail.com>
2026-02-16 14:22:42 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,6 +104,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           { to: '/about', label: 'About', position: 'left' },
+          { to: '/65', label: '65', position: 'left' },
 /*           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
